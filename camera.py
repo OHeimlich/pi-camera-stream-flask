ed3e866291172b334e3f3a561b08439bf3ba70b1
Ori Heimlich <ori.heimlich@zoominfo.com>
2022-07-14 23:40:59 +0300

    support webcam

--- camera.py
+++ camera.py
@@ -3,17 +3,20 @@
 #Desc: This scrtipt script..
 
 import cv2 as cv
-# from imutils.video.pivideostream import PiVideoStream
 import imutils
 import time
 from datetime import datetime
 # import numpy as np
 
+
 class VideoCamera(object):
     def __init__(self, flip = False, file_type  = ".jpg", photo_string= "stream_photo"):
-        # self.vs = PiVideoStream(resolution=(1920, 1080), framerate=30).start()
-        # self.vs = PiVideoStream().start()
-        self.vs = cv.VideoCapture(0)
+        if False:  # only on laptop with webcam
+            self.vs = cv.VideoCapture(0)
+        else:
+            from imutils.video.pivideostream import PiVideoStream
+            # self.vs = PiVideoStream(resolution=(1920, 1080), framerate=30).start()
+            self.vs = PiVideoStream().start()
         self.flip = flip # Flip frame vertically
         self.file_type = file_type # image type i.e. .jpg
         self.photo_string = photo_string # Name to save the photo
@@ -28,8 +31,11 @@ class VideoCamera(object):
         return frame
 
     def get_frame(self):
-        ret, raw_frame = self.vs.read()
-        frame = self.flip_if_needed(raw_frame)
+        if False: # only on laptop with webcam
+            ret, raw_frame = self.vs.read()
+            frame = self.flip_if_needed(raw_frame)
+        else:
+            frame = self.flip_if_needed(self.vs.read())
         ret, jpeg = cv.imencode(self.file_type, frame)
         self.previous_frame = jpeg
         return jpeg.tobytes()
